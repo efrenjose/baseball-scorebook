a272ae4f0811746d3890e0f842f237e724178270
efrenjose <efrenrodriguez@live.com>
2018-11-10 05:57:18 -0600

    add lineup component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatListModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatListModule, 
+    MatFormFieldModule,
+    MatInputModule],
+  exports: [
+    MatButtonModule, 
+    MatCheckboxModule, 
+    MatListModule, 
+    MatFormFieldModule,
+    MatInputModule],
 })
 export class MaterialModule { }
